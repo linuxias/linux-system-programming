34c648bb4824743a3042f2f6c51b54d6ab1ee4e1
Seungha Son <seungha.son@samsung.com>
2018-08-02 13:08:32 +0900

    Adjust CMake build
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

--- server-client/Multithreaded-UDP-model/include/server_type.h
+++ server-client/Multithreaded-UDP-model/include/server_type.h
@@ -3,13 +3,17 @@
 
 #define CLIENT_BUF_MAX 1500
 
-typedef struct _client_h *client_h
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+typedef struct _client_h *client_h;
 
 struct _client_h {
     struct sockaddr_in sock_addr;
     socklen_t sock_len;
-    uintt_t buf[CLIENT_BUF_MAX];
+    uint8_t buf[CLIENT_BUF_MAX];
     ssize_t buf_size;
-}
+};
 
 #endif /* __SERVER_TYPE_H__ */
--- server-client/Multithreaded-UDP-model/src/server.c
+++ server-client/Multithreaded-UDP-model/src/server.c
@@ -1,15 +1,18 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "daemon.h"
 #include "server_type.h"
 
 #define PORT_NUMBER 6200
 #define THREAD_MAX 10
-
-static pthread_t th[THREAD_MAX];
 
 int main()
 {
@@ -18,7 +21,6 @@ int main()
     struct sockaddr_in s_addr;
     struct sockaddr_in c_addr;
     client_h handle = NULL;
-    socklen_t c_addrlen;
 
     s_daemon_become();
 
@@ -28,19 +30,19 @@ int main()
         return -1;
     }
 
-    memset(s_addr, 0, sizeof(struct sockaddr_in));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT_NUMBER);
+    memset(&s_addr, 0, sizeof(struct sockaddr_in));
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_addr.s_addr = INADDR_ANY;
+    s_addr.sin_port = htons(PORT_NUMBER);
 
-    ret = bind(sfd, (struct sockaddr *)&addr, sizeof(addr));
+    ret = bind(sfd, (struct sockaddr *)&s_addr, sizeof(s_addr));
     if (ret == -1) {
         fprintf(stderr, "socket : %s", strerror(errno));
         return -1;
     }
 
     while (1) {
-        memset(c_addr, 0, sizeof(struct sockaddr_in));
+        memset(&c_addr, 0, sizeof(struct sockaddr_in));
 
         handle = NULL;
         handle = (client_h)calloc(1, sizeof(struct _client_h));
