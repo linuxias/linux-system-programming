96e1873c9404e6c11952cccea8684a5328609cf0
Seungha Son <seungha.son@samsung.com>
2018-08-09 19:45:16 +0900

    Init macro directory
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

---
+++ macro/s_macros.h
@@ -0,0 +1,29 @@
+#ifndef __S_MACROS_H__
+#define __S_MACROS_H__
+
+#if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
+#define _S_BOOLEAN_EXPR(expr)                   \
+         S_GNUC_EXTENSION ({                            \
+                    int _s_boolean_var_;                         \
+                    if (expr)                                    \
+                       _s_boolean_var_ = 1;                      \
+                    else                                         \
+                       _s_boolean_var_ = 0;                      \
+                    _s_boolean_var_;                             \
+                 })
+#define S_LIKELY(expr) (__builtin_expect (_S_BOOLEAN_EXPR((expr)), 1))
+#define S_UNLIKELY(expr) (__builtin_expect (_S_BOOLEAN_EXPR((expr)), 0))
+#else
+#define S_LIKELY(expr) (expr)
+#define S_UNLIKELY(expr) (expr)
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#endif /* __S_MACROTS_H__ */
---
+++ macro/s_type_conversion.h
@@ -0,0 +1,9 @@
+#ifndef __S_TYPE_CONVERSION_H__
+#define __S_TYPE_CONVERSION_H__
+
+#define SINT_TO_POINTER(x) ((void *)(long)(x))
+#define SPOINTER_TO_INT(x) ((int)(long)(x))
+#define SUINT_TO_POINTER(x) ((void *)(unsigned long)(x))
+#define SPOINTER_TO_INT(x) ((unsigned int)(unsigned long)(x))
+
+#endif /* __S_TYPE_CONVERSAION_H__*/
