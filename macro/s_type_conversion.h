8c0b8be518f3c1fb6734546a1f5e6a6747387d78
Seungha Son <linuxias@gmail.com>
2018-08-14 22:48:42 +0900

    Move some file to other repository
    Signed-off-by: Seungha Son <linuxias@gmail.com>

--- macro/s_type_conversion.h
+++
@@ -1,9 +0,0 @@
-#ifndef __S_TYPE_CONVERSION_H__
-#define __S_TYPE_CONVERSION_H__
-
-#define SINT_TO_POINTER(x) ((void *)(long)(x))
-#define SPOINTER_TO_INT(x) ((int)(long)(x))
-#define SUINT_TO_POINTER(x) ((void *)(unsigned long)(x))
-#define SPOINTER_TO_INT(x) ((unsigned int)(unsigned long)(x))
-
-#endif /* __S_TYPE_CONVERSAION_H__*/
